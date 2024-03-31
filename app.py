f23182d43b8782f45c50996924aab0bd0d0726cf
nguyen van quang <nguyenvanquang22103@gmail.com>
2024-03-31 10:08:30 +0700

    api face id

---
+++ app.py
@@ -0,0 +1,252 @@
+import cv2
+import os
+from flask import jsonify
+from flask import Flask, request, render_template,redirect, url_for
+from datetime import date
+import mysql.connector
+from datetime import datetime
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import joblib
+import time
+from flask_cors import CORS
+import shutil
+
+app = Flask(__name__)
+CORS(app)
+
+nimgs = 10
+camera_opened = False
+imgBackground = cv2.imread("bg.jpg")
+
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+if not os.path.isdir('static'):
+    os.makedirs('static')
+if not os.path.isdir('static/faces'):
+    os.makedirs('static/faces')
+
+connection = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='social_network'
+)
+cursor = connection.cursor(buffered=True)
+
+def totalreg():
+    return len(os.listdir('static/faces'))
+
+def extract_faces(img):
+    try:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face_points = face_detector.detectMultiScale(gray, 1.2, 5, minSize=(20, 20))
+        return face_points
+    except:
+        return []
+
+def identify_face(facearray):
+    model = joblib.load('static/face_recognition_model.pkl')
+    return model.predict(facearray)
+
+def train_model():
+    faces = []
+    labels = []
+    userlist = os.listdir('static/faces')
+    for user in userlist:
+        for imgname in os.listdir(f'static/faces/{user}'):
+            img = cv2.imread(f'static/faces/{user}/{imgname}')
+            resized_face = cv2.resize(img, (50, 50))
+            faces.append(resized_face.ravel())
+            labels.append(user)
+    faces = np.array(faces)
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(faces, labels)
+    joblib.dump(knn, 'static/face_recognition_model.pkl')
+
+def login_user(name):
+    username, user_id = name.split('_')
+    cursor.execute("SELECT * FROM user_face_regs WHERE user_id = %s", (user_id,))
+    user_record = cursor.fetchone()
+    if user_record:
+        print("Đăng nhập thành công cho người dùng:", user_record[2])
+        return True
+    else:
+        print("Không tìm thấy thông tin người dùng.")
+        return False
+    
+def is_face_registered(user_id):
+    cursor.execute("SELECT * FROM face_ids WHERE user_id = %s", (user_id,))
+    result = cursor.fetchone()
+    if result:
+        return True
+    else:
+        return False
+ 
+def delete_registered_face(user_id, username):
+    try:
+        # Xóa bản ghi từ cơ sở dữ liệu
+        cursor.execute("DELETE FROM face_ids WHERE user_id = %s", (user_id,))
+        cursor.execute("DELETE FROM user_face_regs WHERE user_id = %s", (user_id,))
+        connection.commit()
+        # Xóa hình ảnh từ thư mục
+        user_folder = f'static/faces/{username}_{user_id}'
+        if os.path.exists(user_folder):
+            shutil.rmtree(user_folder)
+            print("Thư mục khuôn mặt đã được xóa.")
+        else:
+            print("Không tìm thấy thư mục khuôn mặt.")
+    except Exception as e:
+        print("Lỗi khi xóa khuôn mặt:", repr(e))
+        connection.rollback()
+
+@app.route('/start', methods=['POST'])
+def start():
+    ret = True
+    count=1
+    cap = cv2.VideoCapture(0)
+    success = False
+    while ret:
+        ret, frame = cap.read()
+        if len(extract_faces(frame)) > 0:
+            (x, y, w, h) = extract_faces(frame)[0]
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 128, 0), 1)
+            face = cv2.resize(frame[y:y+h, x:x+w], (50, 50))
+            identified_person = identify_face(face.reshape(1, -1))[0]
+            if login_user(identified_person):
+                username, user_id = identified_person.split('_')
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 128, 0), 1)
+                cv2.putText(frame, f'{username}', (x,y-15), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 128, 0), 1)
+                success = True
+            else:
+                # Thực hiện hành động sau khi đăng nhập thất bại
+                success = False
+           
+        # Kích thước mới cho hình ảnh cam
+        new_width = 400  # Đặt chiều rộng mới
+        new_height = 400  # Đặt chiều cao mới
+
+        # Thay đổi kích thước của hình ảnh cam
+        small_frame = cv2.resize(frame, (new_width, new_height))
+
+        # Tạo mask trắng có hình dạng hình tròn
+        mask = np.zeros((new_height, new_width), dtype=np.uint8)
+        center_coordinates = (new_width // 2, new_height // 2)
+        radius = min(new_width, new_height) // 2
+        cv2.circle(mask, center_coordinates, radius, (255), -1)
+
+        # Áp dụng mask lên hình ảnh cam
+        masked_frame = cv2.bitwise_and(small_frame, small_frame, mask=mask)
+
+        # Gán hình ảnh cam có khung hình tròn vào imgBackground
+        imgBackground[162:162 + new_height, 55:55 + new_width] = masked_frame
+
+        cv2.imshow('login', imgBackground)
+        if cv2.waitKey(1) == 27:
+        # thoát khỏi vòng lặp
+            break 
+        # đóng cammera
+        count+=1
+        if count > 100:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    if success:
+        return jsonify({'status': True, 'message': 'Xác thực thành công', 'user': {'username': username, 'user_id': user_id}}),200
+    else:
+        return jsonify({'status': False, 'message': 'xác thực thất bại'}),500
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    newusername = request.json['userName']
+    newuserid = request.json['userId']
+    userimagefolder = 'static/faces/' + newusername + '_' + str(newuserid)
+    # Kiểm tra xem người dùng đã tồn tại trong cơ sở dữ liệu chưa
+    cursor.execute("SELECT * FROM user_face_regs WHERE user_id = %s OR user_name = %s", (newuserid, newusername))
+    existing_user = cursor.fetchone()
+    if existing_user:
+        # Người dùng đã tồn tại, trả về phản hồi JSON
+        return jsonify({'status': 'error', 'message': 'Người dùng đã đăng ký khuôn mặt trước đó'}), 400
+    if not os.path.isdir(userimagefolder):
+        os.makedirs(userimagefolder)
+    i, j = 0, 0
+    cap = cv2.VideoCapture(0) 
+    # Thêm người dùng vào cơ sở dữ liệu
+    try:
+        cursor.execute("INSERT INTO user_face_regs (user_id, user_name) VALUES (%s, %s)", (newuserid, newusername))
+        connection.commit()
+        print("Người dùng đã được thêm vào cơ sở dữ liệu.")
+    except Exception as e:
+        print("Lỗi khi thêm người dùng vào cơ sở dữ liệu:", e)
+        connection.rollback()
+    
+    while 1:
+        _, frame = cap.read()
+        faces = extract_faces(frame)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 128, 0), 1)
+            cv2.putText(frame, f'{i}/{nimgs}', (30, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 128, 0), 2, cv2.LINE_AA)
+            if j % 5 == 0:
+                name = newusername + '_' + str(i) + '.jpg'
+                face_image_path = os.path.join(userimagefolder, name)
+                cv2.imwrite(face_image_path, frame[y:y+h, x:x+w])
+                
+                # Lưu đường dẫn hình ảnh khuôn mặt vào cơ sở dữ liệu
+                try:
+                    cursor.execute("INSERT INTO face_ids (user_id, face_image) VALUES (%s, %s)", (newuserid, face_image_path))
+                    connection.commit()
+                    print("Khuôn mặt đã được thêm vào cơ sở dữ liệu.")
+                except Exception as e:
+                    print("Lỗi khi thêm khuôn mặt vào cơ sở dữ liệu:", e)
+                    connection.rollback()
+                i += 1
+            j += 1
+        if j == nimgs*5:
+            break
+          # Kích thước mới cho hình ảnh cam
+        new_width = 400  # Đặt chiều rộng mới
+        new_height = 400  # Đặt chiều cao mới
+
+        # Thay đổi kích thước của hình ảnh cam
+        small_frame = cv2.resize(frame, (new_width, new_height))
+
+        # Tạo mask trắng có hình dạng hình tròn
+        mask = np.zeros((new_height, new_width), dtype=np.uint8)
+        center_coordinates = (new_width // 2, new_height // 2)
+        radius = min(new_width, new_height) // 2
+        cv2.circle(mask, center_coordinates, radius, (255), -1)
+
+        # Áp dụng mask lên hình ảnh cam
+        masked_frame = cv2.bitwise_and(small_frame, small_frame, mask=mask)
+
+        # Gán hình ảnh cam có khung hình tròn vào imgBackground
+        imgBackground[162:162 + new_height, 55:55 + new_width] = masked_frame
+        cv2.imshow('add', imgBackground)
+        if cv2.waitKey(1) == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    print('Training Model')
+    train_model()
+    return jsonify({'status':True, 'message': 'Đăng ký khuôn mặt thành công'})
+
+@app.route('/delete_face', methods=['POST'])
+def delete_face():
+    user_id = request.json['user_id']
+    username = request.json['username']
+    print(user_id)
+    print(username)
+    try:
+        # Kiểm tra xem khuôn mặt đã đăng ký hay chưa
+        if is_face_registered(user_id):
+            # Xóa khuôn mặt đã đăng ký
+            delete_registered_face(user_id, username)
+            return jsonify({'status': 'success', 'message': 'Khuôn mặt đã được xóa thành công'}), 200
+        else:
+            return jsonify({'status': 'error', 'message': 'Khuôn mặt không tồn tại trong cơ sở dữ liệu'}), 404
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
